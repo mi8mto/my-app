d135d992f66fcc05969e6586f5c1a5f33b827c36
Igor Manakhau <mi8mto@gmail.com>
2024-12-02 19:33:59 +0300

    add file app.js without jsx

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
-import logo from './logo.svg'; // декларативный стиль
-import './App.css'; // декларативный стиль
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
 
-// Далее также декларативный стиль
+// export const App = () => {
+// 	const currentYear = new Date().getFullYear();
+
+// 	return (
+// 		<div className="App">
+// 			<header className="App-header">
+// 				<img src={logo} className="App-logo" alt="logo" />
+// 				<p>
+// 					Edit <code>src/App.js</code> and save to reload 2.
+// 				</p>
+// 				<a
+// 					className="App-link"
+// 					href="https://reactjs.org"
+// 					target="_blank"
+// 					rel="noopener noreferrer"
+// 				>
+// 					Learn React
+// 				</a>
+// 				<div className="App-link">{currentYear}</div>
+// 			</header>
+// 		</div>
+// 	);
+// };
+
+// const element = createElement(type, props, ...children)
+
+// далее код без jsx
 export const App = () => {
 	const currentYear = new Date().getFullYear();
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload 2.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<div className="App-link">{currentYear}</div>
-			</header>
-		</div>
-	);
+	return React.createElement('div', { className: 'App' }, [
+		React.createElement(
+			'header',
+			{ className: 'App-header' },
+			React.createElement('img', {
+				src: logo,
+				className: 'App-logo',
+				alt: 'logo',
+			}),
+			React.createElement('p', null, 'Edit src/App.js and save to reload 2.'),
+			React.createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			React.createElement(
+				'div',
+				{
+					className: 'App-link',
+				},
+				currentYear,
+			),
+		),
+	]);
 };
